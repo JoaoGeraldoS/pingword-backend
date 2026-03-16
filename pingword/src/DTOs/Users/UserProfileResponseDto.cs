53b8d73e8fb1d942d1c4cbbd71ce1de20853de65
Joao Geraldo <117011183+JoaoGeraldoS@users.noreply.github.com>
2026-03-16 08:53:03 -0300

    Update UserProfileResponseDto.cs

--- pingword/src/DTOs/Users/UserProfileResponseDto.cs
+++ pingword/src/DTOs/Users/UserProfileResponseDto.cs
@@ -4,11 +4,11 @@ namespace pingword.src.DTOs.Users
 {
     public record UserProfileResponseDto
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public bool IsPremium { get; set; }
-        public string Language {  get; set; }
+        public string Language { get; set; } = string.Empty;
         public UserLevelEnum Level { get; set; }
     }
 }
