240384057b1150e71d1377102c3edee43851e585
Joao Geraldo <117011183+JoaoGeraldoS@users.noreply.github.com>
2026-04-25 19:24:53 -0300

    Update LoginResponseDto.cs

--- pingword/src/DTOs/Users/LoginResponseDto.cs
+++ pingword/src/DTOs/Users/LoginResponseDto.cs
@@ -5,5 +5,6 @@
         public string AccessToken { get; set; } = string.Empty;
         public string? RefreshToken { get; set; }
         public DateTime Expiration { get; set; }
+        public bool IsFirstAccess { get; set; }
     }
 }
